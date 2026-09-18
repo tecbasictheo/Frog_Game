b33dcdaed3c6ea41160af91db850cd9fc6938eb2
tecbasictheo <theodora.matzat@stud.leuphana.de>
2026-09-18 11:04:17 +0200

    text edit

--- frog_game.py
+++ frog_game.py
@@ -206,7 +206,7 @@ def advance_level(screen):
     global current_level, petal_group, LANE_COUNT, jumping, frog_angle, GAME_STATE, game_over
     current_level += 1
     if current_level > len(levels):
-        font = pygame.font.SysFont('Times New Roman', 45)
+        font = pygame.font.SysFont('Times New Roman', 38)
         text_surface = font.render("You win!", True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
         screen.blit(text_surface, text_rect)
@@ -233,7 +233,7 @@ def advance_level(screen):
             petal_group.add(new_petal)
 
     if current_level != 1:
-        font = pygame.font.SysFont('Times New Roman', 45)
+        font = pygame.font.SysFont('Times New Roman', 38)
         text_surface = font.render("Level up!", True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
         screen.blit(text_surface, text_rect)
